d12ff1dc87c96c87ec8ea9e6cf39c5f125512654
盛斌 邓 <dengbinhero@163.com>
2014-12-10 17:05:57 +0800

    Initial Commit

---
+++ TopVCTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TopVCTest
+//
+//  Created by Dengshengbin on 14-12-10.
+//  Copyright (c) 2014年 Dengshengbin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
